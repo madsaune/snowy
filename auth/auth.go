2e74d44aa47b5bc9fb6edb9d734522596eb65198
Mads Moi-Aune <609372+madsaune@users.noreply.github.com>
2022-11-20 01:31:24 +0100

    Auth: Add func for converting username and password to base64 in HTTP Basic Authentication format

--- auth/auth.go
+++ auth/auth.go
@@ -1,7 +1,17 @@
 package auth
 
+import (
+	"encoding/base64"
+	"fmt"
+)
+
 type Authorizer struct {
 	Username    string
 	Password    string
 	InstanceURL string
+}
+
+func (a *Authorizer) ToBase64() string {
+	val := fmt.Sprintf("%s:%s", a.Username, a.Password)
+	return base64.StdEncoding.EncodeToString([]byte(val))
 }
